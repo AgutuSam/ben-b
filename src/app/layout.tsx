89a3802a8a17ab1a92655d82a128644f4debbdd9
AgutuSam <sammyhardcell@gmail.com>
2025-08-12 15:44:20 +0300

    layout "use client"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+import React from 'react';
 import { Nunito } from 'next/font/google'
 import { auth } from '@/app/libs/firebaseHelpers';
 import { onAuthStateChanged } from 'firebase/auth';
